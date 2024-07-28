e3ff79ffd44a7886c9ff588ab38a05216b8cedd1
Nicolás Margenat <nmargenat@itba.edu.ar>
2024-07-28 17:36:16 -0300

    fix(document-manager): wrong merge at some point, correct background tasks function

--- document-manager/src/background_tasks.py
+++ document-manager/src/background_tasks.py
@@ -1,19 +1,11 @@
 from langchain_core.documents import Document
-
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-
 
 
 from src.config.embeddings_db_config import vectorstore
 
 
 def process_document_background(pages, id: int) -> None:
-
-    try:
-        documents = []
-        for page in pages:
-            combined_id = int(f"{id}{page.number}")
-
     text_splitter = RecursiveCharacterTextSplitter(
         chunk_size=700, chunk_overlap=100, length_function=len, is_separator_regex=False
     )
@@ -21,28 +13,19 @@ def process_document_background(pages, id: int) -> None:
     try:
         documents = []
         for page in pages:
-
             documents.append(
                 Document(
                     page_content=page.get_text(),
                     metadata={
-
-                        "id": combined_id,
                         "document_id": id,
                         "page_number": page.number,
                     },
                 )
             )
 
-
-        vectorstore.add_documents(
-            documents, ids=[doc.metadata["id"] for doc in documents]
-        )
-
         split_documents = text_splitter.split_documents(documents)
 
         vectorstore.add_documents(split_documents)
-
     except Exception as e:
         print("Error processing document")
         raise e
