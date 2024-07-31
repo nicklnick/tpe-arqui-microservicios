38c371c6ac9d84fd866a89b7622ab427a00bb4fc
sranucci <sranucci@itba.edu.ar>
2024-07-31 17:10:03 -0300

    fix(chat-session): fix issue with redis

--- chat-session/src/main/java/ar/edu/itba/arquimicro/configuration/RedisConfig.java
+++ chat-session/src/main/java/ar/edu/itba/arquimicro/configuration/RedisConfig.java
@@ -1,5 +1,6 @@
 package ar.edu.itba.arquimicro.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
@@ -10,9 +11,19 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @Configuration
 public class RedisConfig {
 
+
+    @Value("${spring.data.redis.host}")
+    private String hostname;
+
+    @Value("${spring.data.redis.port}")
+    private int port;
+
     @Bean
     public JedisConnectionFactory jedisConnectionFactory() {
-        return new JedisConnectionFactory();
+        JedisConnectionFactory factory =  new JedisConnectionFactory();
+        factory.setHostName(hostname);
+        factory.setPort(port);
+        return factory;
     }
 
     @Bean
