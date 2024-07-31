261faaad3565c6f66f3ec97de6822ba753bec174
saulex16 <scastaneda@itba.edu.ar>
2024-07-31 13:41:37 -0300

    fix(llm-manager): decode rabbit message

--- llm-manager/src/tests/component/mocked_app.py
+++ llm-manager/src/tests/component/mocked_app.py
@@ -1,8 +1,5 @@
-from typing import Callable, Awaitable
+from unittest.mock import patch, MagicMock, AsyncMock
 
-from unittest.mock import patch, MagicMock
-
-from aio_pika import IncomingMessage
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
 
@@ -35,12 +32,6 @@ def configure_mock_provide_vector_store(mock_provide_vector_store):
 
     mock_provide_vector_store.return_value = mock_pgvector
 
-async def custom_parser(msg: IncomingMessage, original_parser: Callable[[IncomingMessage], Awaitable[RabbitMessage]],) -> RabbitMessage:
-    print(msg)
-    print(msg.body)
-    parsed_msg = await original_parser(msg)
-    return parsed_msg
-
 def configure_mock_provide_pgvector_engine(mock_provide_pgvector_engine):
     mock_engine = MagicMock(spec=AsyncEngine)
     mock_provide_pgvector_engine.return_value = mock_engine
@@ -57,44 +48,33 @@ def configure_mock_provide_ai_models_provider(mock_provide_ai_models_provider):
 
 def get_rabbit_router() -> [RabbitRouter,RabbitMQSettings]:
     settings = RabbitMQSettings()
-    return RabbitRouter(settings.build_connection_url(), parser=custom_parser), settings
+    return RabbitRouter(settings.build_connection_url()), settings
 
 router, rabbit_settings = get_rabbit_router()
 
 MOCKED_RESPONSE = 'Paris y torre eiffel'
 
+@apply_types
 @router.subscriber(rabbit_settings.input_queue)
 @router.publisher(rabbit_settings.output_queue)
 async def handle_msg(msg: RabbitMessage) -> ResponseMessage:
     with patch('src.dependencies.provide_vector_store') as mock_provide_vector_store, \
-            patch('src.dependencies.provide_pgvector_engine') as mock_provide_pgvector_engine:
-            #patch('src.dependencies.provide_ai_models_provider') as mock_provide_ai_models_provider, \
-            #patch('src.services.LlmService.create_retrieval_chain', autospec=True) as mock_create_chain:
+            patch('src.dependencies.provide_pgvector_engine') as mock_provide_pgvector_engine, \
+            patch('src.dependencies.provide_ai_models_provider') as mock_provide_ai_models_provider, \
+            patch('src.services.LlmService.create_retrieval_chain', autospec=True) as mock_create_chain:
 
-        #mock_rag_chain = AsyncMock()
-        #mock_rag_chain.ainvoke.return_value = {'answer': MOCKED_RESPONSE}
-        #mock_create_chain.return_value = mock_rag_chain
-
-        print("=============================================================")
-        print(msg)
-        print("-------------------------------------------------------------")
-        print(msg.body)
-        print("=============================================================")
+        mock_rag_chain = AsyncMock()
+        mock_rag_chain.ainvoke.return_value = {'answer': MOCKED_RESPONSE}
+        mock_create_chain.return_value = mock_rag_chain
 
         # Decode the byte string to a regular string
         decoded_body = msg.body.decode('utf-8')
-
-        print("---------------------------------------------------")
-        print(decoded_body)
-        print("---------------------------------------------------")
-        
-        # Deserialize the JSON string to a Python dictionary
         input_msg = InputMessage(**json.loads(decoded_body))
 
 
         configure_mock_provide_vector_store(mock_provide_vector_store)
         configure_mock_provide_pgvector_engine(mock_provide_pgvector_engine)
-        #configure_mock_provide_ai_models_provider(mock_provide_ai_models_provider)
+        configure_mock_provide_ai_models_provider(mock_provide_ai_models_provider)
 
         service: LlmService = provide_llm_service()
         response: ResponseMessage = await service.process_input(input_msg)
